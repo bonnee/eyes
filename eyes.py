a37a52a6ae751d7d7f3cb101c07e62b84a191fd8
Matteo Bonora <bonora.matteo@gmail.com>
2017-03-18 15:37:16 +0100

    Add delay between monitors start

--- eyes.py
+++ eyes.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import time
 import json
 import i3ipc
 
-cmd = 'ffplay -fflags nobuffer -an -framedrop -fast rtsp://'
-#cmd='mplayer --nosound --dr --framedrop --nocache --quiet --fps=5 rtsp://'
+cmd = 'ffplay -fflags nobuffer -an -framedrop -loglevel error -fast -threads 2 -async 1 -preset ultrafast -tune zerolatency -vf scale=640:-1 '
+aft = ' &'
+#cmd='mplayer --nosound --dr --framedrop --nocache --quiet --fps=5 '
+
 with open('streams.json') as data_file:    
     streams = json.load(data_file)
 
-#streams = ['192.168.1.120/live2.sdp', '192.168.1.121/live2.sdp', '192.168.1.122/live2.sdp', '192.168.1.187/live2.sdp']
+os.system('i3-msg "workspace 1"')
+os.system('i3-msg "append_layout $HOME/eyes/layout.json"')
 
 for stream in streams:
-  os.system(cmd+stream + " & > /dev/null")
+  os.system(cmd+stream+aft)
+  time.sleep(1.5);
+
 
 i3 = i3ipc.Connection()
+
+time.sleep(5)
+
+tree = i3.get_tree().leaves()
+for cam in tree:
+  if(cam.workspace().name == '1'):
+    print("Name: %s | Class: %s" % (cam.name, cam.window_class))
 
